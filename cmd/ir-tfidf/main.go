28c1df566d98d4bdf4811162664c107f0d2eae35
Gea-Suan Lin <gslin@gslin.org>
2024-01-31 09:43:30 +0800

    Implement the first part of tfidf.

--- cmd/ir-tfidf/main.go
+++ cmd/ir-tfidf/main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/gslin/go-ir-playground/internal/artifact"
+	"github.com/gslin/go-ir-playground/internal/ngram"
+)
+
 func main() {
+	articles := artifact.Read("data/articles.json")
+
+	for _, article := range articles {
+		title_bag := ngram.Bigram(article.Title)
+		body_bag := ngram.Bigram(article.Body)
+
+		fmt.Printf("title_bag = %v\n", title_bag)
+		fmt.Printf("body_bag = %v\n", body_bag)
+	}
 }
