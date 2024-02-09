18fbfa7292766144e24ff5856d7cc2319ea462e5
Gea-Suan Lin <gslin@gslin.org>
2024-02-09 11:47:13 +0800

    Rename tokenize to tokenizer.

--- cmd/ir-tfidf/main.go
+++ cmd/ir-tfidf/main.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 
 	"github.com/gslin/go-ir-playground/internal/artifact"
-	"github.com/gslin/go-ir-playground/internal/tokenize"
+	"github.com/gslin/go-ir-playground/internal/tokenizer"
 )
 
 func main() {
 	articles := artifact.Read("data/articles.json")
 
 	for _, article := range articles {
-		title_bag := tokenize.Tokenize(article.Title)
-		body_bag := tokenize.Tokenize(article.Body)
+		title_bag := tokenizer.Tokenize(article.Title)
+		body_bag := tokenizer.Tokenize(article.Body)
 
 		fmt.Printf("title_bag = %v\n", title_bag)
 		fmt.Printf("body_bag = %v\n", body_bag)
