57c153a6c304117eead283453c042ee1d8f9686d
Gea-Suan Lin <gslin@gslin.org>
2024-02-16 20:55:23 +0800

    Add more test about bigram.

--- internal/ngram/ngram_test.go
+++ internal/ngram/ngram_test.go
@@ -15,6 +15,12 @@ func testBigram(t *testing.T) {
 	a = ngram.Bigram("測試")
 	assert.Equal(t, len(a), 1)
 	assert.Equal(t, a[0], "測試")
+
+	a = ngram.Bigram("中文測試")
+	assert.Equal(t, len(a), 3)
+	assert.Equal(t, a[0], "中文")
+	assert.Equal(t, a[1], "文測")
+	assert.Equal(t, a[2], "測試")
 }
 
 func TestUnigram(t *testing.T) {
