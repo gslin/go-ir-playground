14e54f239358a1312aab1cb2d53fc26ac02879d7
Gea-Suan Lin <gslin@gslin.org>
2024-02-19 10:41:38 +0800

    Fix test function naming.

--- internal/tokenizer/tokenizer_test.go
+++ internal/tokenizer/tokenizer_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gslin/go-ir-playground/internal/tokenizer"
 )
 
-func testTokenize(t *testing.T) {
+func TestTokenize(t *testing.T) {
 	a := tokenizer.Tokenize("test")
 	assert.Equal(t, len(a), 1)
 	assert.Equal(t, a[0], "test")
