7290880a1012f7b9e643d23b11fc52fb1d7ed164
Gea-Suan Lin <gslin@gslin.org>
2024-02-28 15:37:00 +0800

    Add news-cna.

--- cmd/news-cna/main.go
+++ cmd/news-cna/main.go
@@ -1,4 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
 func main() {
+	url := "https://www.cna.com.tw/list/aall.aspx"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		log.Fatalf("res.StatusCode: %v", resp.StatusCode)
+	}
+
+	// #jsMainList li
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	doc.Find("#jsMainList li").Each(func (i int, li *goquery.Selection) {
+		href, exists := li.Find("a").Attr("href")
+		if !exists {
+			return
+		}
+
+		title := li.Find("h2").Text()
+
+		fmt.Printf("%v: %s\n", href, title)
+	})
 }
